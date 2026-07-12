c572ccd7e8b0c14319e04c9f49b295de743f57c2
Borislav Semerdzhiev <borislavsemerdzhiev.02@gmail.com>
2026-07-13 01:47:23 +0300

    Adds edu-187 A-D

---
+++ codeforces/edu-187/C.cpp
@@ -0,0 +1,215 @@
+#include <bits/stdc++.h>
+#include <chrono>
+#include <codecvt>
+#include <cstdio>
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+using pi = std::pair<int32_t, int32_t>;
+using vi = std::vector<int32_t>;
+using str = std::string;
+using db = double;
+using ld = long double;
+
+// templates
+#define tcT template <class T
+#define tcTU tcT, class U
+#define tcTUU tcT, class... U
+
+// pairs
+#define mp make_pair
+#define f first
+#define s second
+
+// loops
+#define FOR(i, a, b) for (int32_t i = (a); i < (b); i++)
+#define ROF(i, a, b) for (int32_t i = (b) - 1; i >= (a); i--)
+#define trav(a, x) for (auto &(a) : (x))
+
+// vectors
+#define sz(a) (int32_t)(a).size()
+#define all(x) (x).begin(), (x).end()
+#define sor(x) sort(all(x))
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+
+const int32_t MOD = 998244353;
+const int32_t MAXN = 2e5 + 5;
+const int64_t INF = 1e18;
+const double PI = acos(-1);
+const int32_t tSZ = (1 << 21);
+const int32_t dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1}; // for grid problems
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+
+constexpr int pcnt(int x) { return __builtin_popcount(x); } // number of bits
+
+namespace MO {
+int32_t add(int64_t x, int64_t y) {
+  return ((x % MOD) + (y % MOD) + MOD) % MOD;
+}
+int32_t mul(int64_t x, int64_t y) { return ((x % MOD) * (y % MOD)) % MOD; }
+
+int32_t pow(int32_t x, int32_t y) {
+  int32_t res = 1;
+
+  while (y) {
+    if (y & 1) {
+      res = mul(res, x);
+    }
+    x = mul(x, x);
+    y >>= 1;
+  }
+  return res;
+}
+
+int32_t inverse(int32_t x) { return pow(x, MOD - 2); }
+}; // namespace MO
+
+// input
+tcT > void re(complex<T> &c);
+tcTU > void re(pair<T, U> &p);
+tcT > void re(vector<T> &v);
+
+tcT > void re(T &x) { cin >> x; }
+void re(db &d) {
+  str t;
+  cin >> t;
+  d = stod(t);
+}
+void re(ld &d) {
+  str t;
+  cin >> t;
+  d = stold(t);
+}
+tcTUU > void re(T &x, U &...u) {
+  re(x);
+  re(u...);
+}
+
+tcT > void re(complex<T> &x) {
+  T a, b;
+  re(a, b);
+  x = {a, b};
+}
+tcTU > void re(pair<T, U> &p) { re(p.f, p.s); }
+tcT > void re(vector<T> &v) { trav(x, v) re(x); }
+
+// to string
+#define ts to_string
+str ts(char c) { return str(1, c); }
+str ts(const char *s) { return (str)s; }
+str ts(str s) { return s; }
+str ts(bool x) { return x ? "YES" : "NO"; }
+
+// output
+tcT > void pr(const T &x) { cout << ts(x); }
+tcTUU > void pr(const T &t, const U &...u) {
+  pr(t);
+  pr(u...);
+}
+void ps() { pr("\n"); }
+tcTUU > void ps(const T &t, const U &...u) {
+  pr(t);
+  if (sizeof...(u)) {
+    pr(" ");
+  }
+  ps(u...);
+}
+
+void setIn(const str &f_name) { freopen(f_name.c_str(), "r", stdin); }
+void setOut(const str &f_name) { freopen(f_name.c_str(), "w", stdout); }
+void unsyncIO() { cin.tie(0)->sync_with_stdio(false); }
+void setIO(str st = "") {
+  unsyncIO();
+  if (sz(st)) {
+    setIn(st + ".in"), setOut(st + ".out");
+  }
+}
+
+// DEBUG
+void DBG() { cerr << "]\n"; }
+tcTUU > void DBG(const T &t, const U &...u) {
+  cerr << ts(t);
+  if (sizeof...(u)) {
+    cerr << ", ";
+  }
+  DBG(u...);
+}
+
+int64_t s, m;
+
+bool check(int64_t x) {
+  vector<bool> vis(64, 0);
+  vector<int64_t> bonus_av(64, 0);
+
+  for (int32_t i{0}; i < 64; i++) {
+    if (m & (1LL << i)) {
+      int64_t av = x + bonus_av[i];
+
+      for (int32_t j{i}; j < 64; j++) {
+        if ((j != i) && (m & (1LL << j))) {
+          bonus_av[j] += av >> (j - i);
+          break;
+        }
+        if ((s & (1LL << j)) && !vis[j]) {
+          int64_t needed = (1LL << (j - i));
+
+          if (av >= needed) {
+            vis[j] = 1;
+            av -= needed;
+          }
+        }
+      }
+    }
+  }
+  for (int32_t i{0}; i < 63; i++) {
+    if (!vis[i] && ((1LL << i) & s)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void solve() {
+  int64_t ans = -1;
+  int64_t l = 1, r = INF;
+
+  // ps(check(642));
+
+  while (l <= r) {
+    int64_t middle = (l + r) >> 1;
+
+    if (check(middle)) {
+      r = middle - 1;
+      ans = middle;
+    } else {
+      l = middle + 1;
+    }
+  }
+
+  ps(ans);
+}
+
+int main() {
+  setIO();
+
+  size_t t;
+  cin >> t;
+
+  while (t--) {
+    re(s, m);
+
+    solve();
+  }
+
+  return 0;
+}
---
+++ codeforces/edu-187/D.cpp
@@ -0,0 +1,244 @@
+#include <bits/stdc++.h>
+#include <chrono>
+#include <cstdint>
+#include <cstdio>
+#include <iostream>
+#include <random>
+#include <unordered_map>
+
+using namespace std;
+
+using pi = std::pair<int32_t, int32_t>;
+using vi = std::vector<int32_t>;
+using str = std::string;
+using db = double;
+using ld = long double;
+
+// templates
+#define tcT template <class T
+#define tcTU tcT, class U
+#define tcTUU tcT, class... U
+
+// pairs
+#define mp make_pair
+#define f first
+#define s second
+
+// loops
+#define FOR(i, a, b) for (int32_t i = (a); i < (b); i++)
+#define ROF(i, a, b) for (int32_t i = (b) - 1; i >= (a); i--)
+#define trav(a, x) for (auto &(a) : (x))
+
+// vectors
+#define sz(a) (int32_t)(a).size()
+#define all(x) (x).begin(), (x).end()
+#define sor(x) sort(all(x))
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+
+const int32_t MOD = 998244353;
+const int32_t MAXN = 1e6 + 5;
+const int64_t INF = 1e18;
+const double PI = acos(-1);
+const int32_t tSZ = (1 << 21);
+const int32_t dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1}; // for grid problems
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+
+constexpr int pcnt(int x) { return __builtin_popcount(x); } // number of bits
+
+namespace MO {
+int32_t add(int64_t x, int64_t y) {
+  return ((x % MOD) + (y % MOD) + MOD) % MOD;
+}
+int32_t mul(int64_t x, int64_t y) { return ((x % MOD) * (y % MOD)) % MOD; }
+
+int32_t pow(int32_t x, int32_t y) {
+  int32_t res = 1;
+
+  while (y) {
+    if (y & 1) {
+      res = mul(res, x);
+    }
+    x = mul(x, x);
+    y >>= 1;
+  }
+  return res;
+}
+
+int32_t inverse(int32_t x) { return pow(x, MOD - 2); }
+}; // namespace MO
+
+// input
+tcT > void re(complex<T> &c);
+tcTU > void re(pair<T, U> &p);
+tcT > void re(vector<T> &v);
+
+tcT > void re(T &x) { cin >> x; }
+void re(db &d) {
+  str t;
+  cin >> t;
+  d = stod(t);
+}
+void re(ld &d) {
+  str t;
+  cin >> t;
+  d = stold(t);
+}
+tcTUU > void re(T &x, U &...u) {
+  re(x);
+  re(u...);
+}
+
+tcT > void re(complex<T> &x) {
+  T a, b;
+  re(a, b);
+  x = {a, b};
+}
+tcTU > void re(pair<T, U> &p) { re(p.f, p.s); }
+tcT > void re(vector<T> &v) { trav(x, v) re(x); }
+
+// to string
+#define ts to_string
+str ts(char c) { return str(1, c); }
+str ts(const char *s) { return (str)s; }
+str ts(str s) { return s; }
+str ts(bool x) { return x ? "YES" : "NO"; }
+
+// output
+tcT > void pr(const T &x) { cout << ts(x); }
+tcTUU > void pr(const T &t, const U &...u) {
+  pr(t);
+  pr(u...);
+}
+void ps() { pr("\n"); }
+tcTUU > void ps(const T &t, const U &...u) {
+  pr(t);
+  if (sizeof...(u)) {
+    pr(" ");
+  }
+  ps(u...);
+}
+
+void setIn(const str &f_name) { freopen(f_name.c_str(), "r", stdin); }
+void setOut(const str &f_name) { freopen(f_name.c_str(), "w", stdout); }
+void unsyncIO() { cin.tie(0)->sync_with_stdio(false); }
+void setIO(str st = "") {
+  unsyncIO();
+  if (sz(st)) {
+    setIn(st + ".in"), setOut(st + ".out");
+  }
+}
+
+// DEBUG
+void DBG() { cerr << "]\n"; }
+tcTUU > void DBG(const T &t, const U &...u) {
+  cerr << ts(t);
+  if (sizeof...(u)) {
+    cerr << ", ";
+  }
+  DBG(u...);
+}
+
+int32_t n, m, a[MAXN], b[MAXN];
+bool is_in[MAXN * 2];
+vi primes;
+
+int32_t rec(int32_t ind, const vector<pi> &vv, int32_t cur_prod) {
+  if (ind == sz(vv)) {
+    return is_in[cur_prod];
+  }
+  int32_t prod = 1;
+  int32_t ans = 0;
+  for (int32_t i{0}; i <= vv[ind].second; i++) {
+    ans += rec(ind + 1, vv, cur_prod * prod);
+    prod *= vv[ind].first;
+  }
+  return ans;
+}
+
+void solve() {
+  for (int32_t i{1}; i <= n + m; i++) {
+    is_in[i] = 0;
+  }
+  int32_t k = 0;
+  FOR(i, 0, n) {
+    if (is_in[a[i]] == 0) {
+      is_in[a[i]] = 1;
+      k++;
+    }
+  }
+
+  int32_t br[3] = {0};
+
+  vector<int32_t> is_divided(n + m + 1, 0);
+
+  FOR(i, 1, n + m + 1) {
+    if (is_in[i])
+      for (int32_t j = i; j <= n + m; j += i) {
+        is_divided[j] += 1;
+      }
+  }
+  for (int32_t i{0}; i < m; i++) {
+    int32_t k2 = is_divided[b[i]];
+
+    if (k2 == 0) {
+      br[1]++;
+    } else if (k2 == k) {
+      br[0]++;
+    } else {
+      br[2]++;
+    }
+  }
+
+  int32_t p = 0;
+  if (br[2] & 1) {
+    p = 1;
+  }
+  if (br[1] > br[0]) {
+    ps("Bob");
+  } else if (br[1] == br[0]) {
+    ps(p ? "Alice" : "Bob");
+  } else {
+    ps("Alice");
+  }
+}
+
+void precalc() {
+  vector<bool> non_prime(MAXN * 2, 0);
+
+  // primes.push_back(1);
+  for (int32_t i{2}; i < MAXN * 2; i++) {
+    if (!non_prime[i]) {
+      primes.push_back(i);
+      for (int64_t j(1LL * i * i); j < 2 * MAXN; j += i) {
+        non_prime[j] = 1;
+      }
+    }
+  }
+}
+
+int main() {
+  setIO();
+
+  size_t t;
+  cin >> t;
+
+  // precalc();
+
+  while (t--) {
+    re(n, m);
+    FOR(i, 0, n) { re(a[i]); }
+
+    FOR(i, 0, m) { re(b[i]); }
+    solve();
+  }
+
+  return 0;
+}
