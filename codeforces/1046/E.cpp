0fb65b0b44d7f90e295eaf7105551fcdb8d4e9b6
Borislav Semerdzhiev <borislavsemerdzhiev.02@gmail.com>
2026-09-05 01:21:16 +0200

    Adds 1046/A-E

---
+++ codeforces/1046/E.cpp
@@ -0,0 +1,220 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using pi = std::pair<int32_t, int32_t>;
+using pl = std::pair<int64_t, int64_t>;
+using vi = std::vector<int32_t>;
+using vl = std::vector<int64_t>;
+using vpi = std::vector<pi>;
+using vpl = std::vector<pl>;
+using str = std::string;
+using db = double;
+using ld = long double;
+
+// templates
+#define tcT template <class T
+#define tcTU tcT, class U
+#define tcTUU tcT, class... U
+
+// pairs
+#define mp make_pair
+#define f first
+#define s second
+
+// loops
+#define FOR(i, a, b) for (int32_t i = (a); i < (b); i++)
+#define ROF(i, a, b) for (int32_t i = (b) - 1; i >= (a); i--)
+#define trav(a, x) for (auto &(a) : (x))
+
+// vectors
+#define sz(a) (int32_t)(a).size()
+#define all(x) (x).begin(), (x).end()
+#define sor(x) sort(all(x))
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+
+const int32_t MOD = 998244353;
+const int32_t MAXN = 2e5 + 5;
+const int64_t INF = 1e18;
+const double PI = acos(-1);
+const int32_t tSZ = (1 << 21);
+const int32_t dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1}; // for grid problems
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+
+constexpr int pcnt(int x) { return __builtin_popcount(x); } // number of bits
+
+namespace MO {
+int32_t add(int64_t x, int64_t y) {
+  return ((x % MOD) + (y % MOD) + MOD) % MOD;
+}
+int32_t mul(int64_t x, int64_t y) { return ((x % MOD) * (y % MOD)) % MOD; }
+
+int32_t pow(int32_t x, int32_t y) {
+  int32_t res = 1;
+
+  while (y) {
+    if (y & 1) {
+      res = mul(res, x);
+    }
+    x = mul(x, x);
+    y >>= 1;
+  }
+  return res;
+}
+
+int32_t inverse(int32_t x) { return pow(x, MOD - 2); }
+}; // namespace MO
+
+// input
+tcT > void re(complex<T> &c);
+tcTU > void re(pair<T, U> &p);
+tcT > void re(vector<T> &v);
+
+tcT > void re(T &x) { cin >> x; }
+void re(db &d) {
+  str t;
+  cin >> t;
+  d = stod(t);
+}
+void re(ld &d) {
+  str t;
+  cin >> t;
+  d = stold(t);
+}
+tcTUU > void re(T &x, U &...u) {
+  re(x);
+  re(u...);
+}
+
+tcT > void re(complex<T> &x) {
+  T a, b;
+  re(a, b);
+  x = {a, b};
+}
+tcTU > void re(pair<T, U> &p) { re(p.f, p.s); }
+tcT > void re(vector<T> &v) { trav(x, v) re(x); }
+
+// to string
+#define ts to_string
+str ts(char c) { return str(1, c); }
+str ts(const char *s) { return (str)s; }
+str ts(str s) { return s; }
+str ts(bool x) { return x ? "YES" : "NO"; }
+
+// output
+tcT > void pr(const T &x) { cout << ts(x); }
+tcTUU > void pr(const T &t, const U &...u) {
+  pr(t);
+  pr(u...);
+}
+void ps() { pr("\n"); }
+tcTUU > void ps(const T &t, const U &...u) {
+  pr(t);
+  if (sizeof...(u)) {
+    pr(" ");
+  }
+  ps(u...);
+}
+
+void setIn(const str &f_name) { freopen(f_name.c_str(), "r", stdin); }
+void setOut(const str &f_name) { freopen(f_name.c_str(), "w", stdout); }
+void unsyncIO() { cin.tie(0)->sync_with_stdio(false); }
+void setIO(str st = "") {
+  unsyncIO();
+  if (sz(st)) {
+    setIn(st + ".in"), setOut(st + ".out");
+  }
+}
+
+// DEBUG
+void DBG() { cerr << "]\n"; }
+tcTUU > void DBG(const T &t, const U &...u) {
+  cerr << ts(t);
+  if (sizeof...(u)) {
+    cerr << ", ";
+  }
+  DBG(u...);
+}
+
+int64_t n, x[MAXN], y[MAXN];
+
+int64_t jmp(char dir, int64_t k) {
+  ps("?", dir, k);
+  cout.flush();
+  int64_t resp;
+  re(resp);
+  if (resp == -1) {
+    exit(0);
+  }
+  return resp;
+}
+
+int32_t get_closest(int64_t X, int64_t Y) {
+  int64_t sm = INT64_MAX;
+  int32_t to_ret;
+  FOR(i, 0, n) {
+    if (abs(X - x[i]) + abs(Y - y[i]) < sm) {
+      sm = abs(X - x[i]) + abs(Y - y[i]);
+      to_ret = i;
+    }
+  }
+  return to_ret;
+}
+
+void solve() {
+  int32_t left_low = get_closest(-2e9, -2e9);
+  int32_t right_low = get_closest(2e9, -2e9);
+
+  jmp('D', int64_t(1e9));
+  jmp('D', int64_t(1e9));
+
+  jmp('L', int64_t(1e9));
+
+  int64_t s1 = jmp('L', int64_t(1e9));
+
+  jmp('R', int64_t(1e9));
+  jmp('R', int64_t(1e9));
+
+  jmp('R', int64_t(1e9));
+
+  int64_t s2 = jmp('R', int64_t(1e9));
+
+  int64_t delta_y = (s1 + s2 - int64_t(4e9) + (x[right_low] - x[left_low]) -
+                     (y[right_low] - y[left_low])) /
+                    2LL;
+
+  int64_t real_y = y[left_low] - delta_y;
+
+  int64_t real_x = x[left_low] - (s1 - delta_y);
+
+  ps("!", real_x + int64_t(2e9), real_y + int64_t(2e9));
+  cout.flush();
+}
+
+int main() {
+  setIO();
+
+  size_t t;
+  re(t);
+
+  while (t--) {
+    re(n);
+
+    if (n == -1) {
+      exit(0);
+    }
+    FOR(i, 0, n) { re(x[i], y[i]); }
+
+    solve();
+  }
+
+  return 0;
+}
